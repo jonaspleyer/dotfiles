17b49638fbd3838bc2b593bb5144fb374fc1fb54
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-07-17 05:22:59 +0200

    change order such that packer is installed first if not already done

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,8 @@
+require('plugins')
 require('alexzyl-nordic')
 require('flake8')
 require('lewis6991-gitsigns')
 require('mason-config')
-require('plugins')
 require('rust-analyzer')
 require("set")
 
