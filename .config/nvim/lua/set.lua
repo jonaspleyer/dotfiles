bb72c51edb9c28d702d4016b4ab81c29ab0cd567
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-08-02 04:15:48 +0200

    do not use relative line numbering anymore

--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -8,7 +8,7 @@ vim.opt.softtabstop=4           --see multiple spaces as tabstops so <BS> does t
 vim.opt.expandtab=true          --converts tabs to white space
 vim.opt.shiftwidth=4            --width for autoindents
 vim.opt.autoindent=true         --indent a new line the same amount as the line just typed
-vim.opt.relativenumber=true     --add line numbers
+--vim.opt.relativenumber=true     --add line numbers
 vim.g.netrw_bufsettings = 'noma nomod nu rnu nobl nowrap ro'
                                 --add relative numbers for netrw as well
 vim.opt.wildmode=longest,list   --get bash-like tab completions
