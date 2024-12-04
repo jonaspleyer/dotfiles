cd1d8e1b4fc68dc77d183712c068f2da01b5cbf9
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-12-04 17:37:20 +0100

    formatting

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,12 +1,12 @@
 return {
-    'lewis6991/gitsigns.nvim',
-    ft = 'gitsigns',
+    "lewis6991/gitsigns.nvim",
+    ft = "gitsigns",
     lazy = false,
     opts = {
         numhl = true,
         signcolumn = false,
     },
     init = function()
-        vim.api.nvim_set_hl(0, 'GitSignsAddLn', { link = 'GitSignsChangeLn' })
+        vim.api.nvim_set_hl(0, "GitSignsAddLn", { link = "GitSignsChangeLn" })
     end,
 }
