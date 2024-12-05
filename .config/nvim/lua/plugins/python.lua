5556e9497339db1b9e1977d3b9c0863e3a855057
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-12-05 02:10:12 +0100

    remove neotest

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -98,22 +98,7 @@ return {
             end
         end,
     },
-    {
-        "nvim-neotest/neotest",
-        optional = true,
-        dependencies = {
-            "nvim-neotest/neotest-python",
-        },
-        opts = {
-            adapters = {
-                ["neotest-python"] = {
-                    -- Here you can specify the settings for the adapter, i.e.
-                    -- runner = "pytest",
-                    -- python = ".venv/bin/python",
-                },
-            },
-        },
-    },
+
     {
         "mfussenegger/nvim-dap",
         optional = true,
