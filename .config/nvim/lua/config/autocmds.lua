37a80faf4d54e1a1393ee935d97a6d508e080d51
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-12-05 22:05:03 +0100

    add typst support

---
+++ .config/nvim/lua/config/autocmds.lua
@@ -0,0 +1,10 @@
+vim.api.nvim_create_autocmd({
+    "BufNewFile",
+    "BufRead",
+}, {
+    pattern = "*.typ",
+    callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+        vim.api.nvim_set_option_value("filetype", "typst", { buf = buf })
+    end,
+})
