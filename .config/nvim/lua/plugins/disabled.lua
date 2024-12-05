46f9b1dbca590048a93637d904852f58970e864b
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2024-12-05 12:16:14 +0100

    correctly enable cmp; use separate file for that.

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,7 +3,7 @@ return {
     { "catppuccin/nvim", enabled = false },
     { "stevearc/dressing.nvim", enabled = false },
     { "folke/flash.nvim", enabled = false },
-    { "garymjr/nvim-snippets", enabled = false },
+    -- { "garymjr/nvim-snippets", enabled = false },
     { "folke/trouble.nvim", enabled = false },
     { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
