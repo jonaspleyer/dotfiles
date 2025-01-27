5bb6393826787e7bb7c8fc2dbab7346cb955ad44
Jonas Pleyer <jonas.pleyer@fdm.uni-freiburg.de>
2025-01-27 10:58:45 +0100

    rust-analyzer; do not display chaining hints

--- .config/nvim/lua/plugins/rust.lua
+++ .config/nvim/lua/plugins/rust.lua
@@ -81,6 +81,7 @@ return {
                                     enable = false,
                                 },
                             },
+                            chainingHints = false,
                         },
                     },
                 },
