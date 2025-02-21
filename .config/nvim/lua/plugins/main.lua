580eeed18b7234a192db3594e83f54b653af4d56
Jonas Pleyer <jonas.dev@pleyer.org>
2025-02-21 10:53:40 +0100

    change order; move to top of file

--- .config/nvim/lua/plugins/main.lua
+++ .config/nvim/lua/plugins/main.lua
@@ -9,6 +9,14 @@ if true then return {} end
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
+    -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+    -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- use mini.starter instead of alpha
+    { import = "lazyvim.plugins.extras.ui.mini-starter" },
+    -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
+    { import = "lazyvim.plugins.extras.lang.json" },
+
     -- add gruvbox
     { "ellisonleao/gruvbox.nvim" },
 
@@ -94,10 +102,6 @@ return {
         },
     },
 
-    -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-    -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-
     -- add more treesitter parsers
     {
         "nvim-treesitter/nvim-treesitter",
@@ -160,12 +164,6 @@ return {
             }
         end,
     },
-
-    -- use mini.starter instead of alpha
-    { import = "lazyvim.plugins.extras.ui.mini-starter" },
-
-    -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-    { import = "lazyvim.plugins.extras.lang.json" },
 
     -- add any tools you want to have installed below
     {
