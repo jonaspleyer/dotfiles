e162333d9ac7ac68efe038c07bae5c23c12765bb
Jonas Pleyer <jonas.dev@pleyer.org>
2025-09-10 05:35:10 +0200

    new settings for python

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -2,7 +2,6 @@ if lazyvim_docs then
     -- LSP Server to use for Python.
     -- Set to "basedpyright" to use basedpyright instead of pyright.
     vim.g.lazyvim_python_lsp = "basedpyright"
-    -- Set to "ruff_lsp" to use the old LSP implementation version.
     vim.g.lazyvim_python_ruff = "ruff"
 end
 
@@ -23,49 +22,6 @@ return {
             },
         })
     end,
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = { ensure_installed = { "ninja", "rst" } },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-                ruff = {
-                    cmd_env = { RUFF_TRACE = "messages" },
-                    init_options = {
-                        settings = {
-                            logLevel = "error",
-                        },
-                    },
-                    keys = {
-                        {
-                            "<leader>co",
-                            LazyVim.lsp.action["source.organizeImports"],
-                            desc = "Organize Imports",
-                        },
-                    },
-                },
-                ruff_lsp = {
-                    keys = {
-                        {
-                            "<leader>co",
-                            LazyVim.lsp.action["source.organizeImports"],
-                            desc = "Organize Imports",
-                        },
-                    },
-                },
-            },
-            setup = {
-                [ruff] = function()
-                    LazyVim.lsp.on_attach(function(client, _)
-                        -- Disable hover in favor of Pyright
-                        client.server_capabilities.hoverProvider = false
-                    end, ruff)
-                end,
-            },
-        },
-    },
 
     {
         "neovim/nvim-lspconfig",
@@ -89,6 +45,42 @@ return {
     },
 
     {
+        "nvim-treesitter/nvim-treesitter",
+        opts = { ensure_installed = { "ninja", "rst" } },
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                ruff = {
+                    cmd_env = { RUFF_TRACE = "messages" },
+                    init_options = {
+                        settings = {
+                            logLevel = "error",
+                        },
+                    },
+                    keys = {
+                        {
+                            "<leader>co",
+                            LazyVim.lsp.action["source.organizeImports"],
+                            desc = "Organize Imports",
+                        },
+                    },
+                },
+            },
+            setup = {
+                [ruff] = function()
+                    LazyVim.lsp.on_attach(function(client, _)
+                        -- Disable hover in favor of Pyright
+                        client.server_capabilities.hoverProvider = false
+                    end, ruff)
+                end,
+            },
+        },
+    },
+
+    {
         "neovim/nvim-lspconfig",
         opts = function(_, opts)
             local servers = { "pyright", "basedpyright", "ruff", "ruff_lsp", ruff, lsp }
@@ -100,65 +92,22 @@ return {
     },
 
     {
-        "mfussenegger/nvim-dap",
-        optional = true,
-        dependencies = {
-            "mfussenegger/nvim-dap-python",
-      -- stylua: ignore
-      keys = {
-        { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
-        { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
-      },
-            config = function()
-                if vim.fn.has("win32") == 1 then
-                    require("dap-python").setup(
-                        LazyVim.get_pkg_path("debugpy", "/venv/Scripts/pythonw.exe")
-                    )
-                else
-                    require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/bin/python"))
-                end
-            end,
-        },
-    },
-
-    {
         "linux-cultist/venv-selector.nvim",
-        branch = "regexp", -- Use this branch for the new version
-        cmd = "VenvSelect",
-        enabled = function()
-            return LazyVim.has("telescope.nvim")
-        end,
-        opts = {
-            settings = {
-                options = {
-                    notify_user_on_venv_activation = true,
-                },
-            },
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            {
+                "nvim-telescope/telescope.nvim",
+                branch = "0.1.x",
+                dependencies = { "nvim-lua/plenary.nvim" },
+            }, -- optional: you can also use fzf-lua, snacks, mini-pick instead.
         },
-        --  Call config for python files and load the cached venv automatically
-        ft = "python",
+        ft = "python", -- Load when opening Python files
         keys = {
-            { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" },
+            { ",v", "<cmd>VenvSelect<cr>" }, -- Open picker on keymap
         },
-    },
-
-    {
-        "hrsh7th/nvim-cmp",
-        optional = true,
-        opts = function(_, opts)
-            opts.auto_brackets = opts.auto_brackets or {}
-            table.insert(opts.auto_brackets, "python")
-        end,
-    },
-
-    -- Don't mess up DAP adapters provided by nvim-dap-python
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        optional = true,
-        opts = {
-            handlers = {
-                python = function() end,
-            },
+        opts = { -- this can be an empty lua table - just showing below for clarity.
+            search = {}, -- if you add your own searches, they go here.
+            options = {}, -- if you add plugin options, they go here.
         },
     },
 }
