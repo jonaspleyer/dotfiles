eba135fb30d89c39d0c630907054acf554d286ba
Jonas Pleyer <jonas.dev@pleyer.org>
2025-10-29 23:01:23 +0100

    remove typescript support

--- .config/nvim/lua/plugins/main.lua
+++ .config/nvim/lua/plugins/main.lua
@@ -7,11 +7,12 @@
 return {
     -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
     -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- use mini.starter instead of alpha
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
     -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
     { import = "lazyvim.plugins.extras.lang.json" },
+    -- { import = "lazyvim.plugins.extras.lang.cmake" },
 
     -- add gruvbox
     { "ellisonleao/gruvbox.nvim" },
@@ -59,44 +60,44 @@ return {
     },
 
     -- add tsserver and setup with typescript.nvim instead of lspconfig
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "jose-elias-alvarez/typescript.nvim",
-            init = function()
-                require("lazyvim.util").lsp.on_attach(function(_, buffer)
-          -- stylua: ignore
-          vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-                    vim.keymap.set(
-                        "n",
-                        "<leader>cR",
-                        "TypescriptRenameFile",
-                        { desc = "Rename File", buffer = buffer }
-                    )
-                end)
-            end,
-        },
-        ---@class PluginLspOpts
-        opts = {
-            ---@type lspconfig.options
-            servers = {
-                -- tsserver will be automatically installed with mason and loaded with lspconfig
-                tsserver = {},
-            },
-            -- you can do any additional lsp server setup here
-            -- return true if you don't want this server to be setup with lspconfig
-            ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-            setup = {
-                -- example to setup with typescript.nvim
-                tsserver = function(_, opts)
-                    require("typescript").setup({ server = opts })
-                    return true
-                end,
-                -- Specify * to use this function as a fallback for any server
-                -- ["*"] = function(server, opts) end,
-            },
-        },
-    },
+    -- {
+    --     "neovim/nvim-lspconfig",
+    --     dependencies = {
+    --         "jose-elias-alvarez/typescript.nvim",
+    --         init = function()
+    --             require("lazyvim.util").lsp.on_attach(function(_, buffer)
+    --       -- stylua: ignore
+    --       vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+    --                 vim.keymap.set(
+    --                     "n",
+    --                     "<leader>cR",
+    --                     "TypescriptRenameFile",
+    --                     { desc = "Rename File", buffer = buffer }
+    --                 )
+    --             end)
+    --         end,
+    --     },
+    --     ---@class PluginLspOpts
+    --     opts = {
+    --         ---@type lspconfig.options
+    --         servers = {
+    --             -- tsserver will be automatically installed with mason and loaded with lspconfig
+    --             tsserver = {},
+    --         },
+    --         -- you can do any additional lsp server setup here
+    --         -- return true if you don't want this server to be setup with lspconfig
+    --         ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+    --         setup = {
+    --             -- example to setup with typescript.nvim
+    --             tsserver = function(_, opts)
+    --                 require("typescript").setup({ server = opts })
+    --                 return true
+    --             end,
+    --             -- Specify * to use this function as a fallback for any server
+    --             -- ["*"] = function(server, opts) end,
+    --         },
+    --     },
+    -- },
 
     -- add more treesitter parsers
     {
@@ -115,7 +116,7 @@ return {
                 "rust",
                 "ron",
                 "tsx",
-                "typescript",
+                -- "typescript",
                 "vim",
                 "wgsl",
                 "yaml",
@@ -132,7 +133,7 @@ return {
             -- add tsx and treesitter
             vim.list_extend(opts.ensure_installed, {
                 "tsx",
-                "typescript",
+                -- "typescript",
             })
         end,
     },
